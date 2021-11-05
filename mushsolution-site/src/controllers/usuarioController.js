94b852ce09bfe68e753b32404f3d36737f943748
renata480 <renata.melo@bandtec.com.br>
2021-11-05 16:45:41 -0300

    atualizando Node Api

---
+++ mushsolution-site/src/controllers/usuarioController.js
@@ -0,0 +1,100 @@
+var usuarioModel = require("../models/usuarioModel");
+
+var sessoes = [];
+
+function testar(req, res) {
+    console.log("ENTRAMOS NA usuarioController");
+    res.json("ESTAMOS FUNCIONANDO!");
+}
+
+function listar(req, res) {
+    usuarioModel.listar()
+        .then(function (resultado) {
+            if (resultado.length > 0) {
+                res.status(200).json(resultado);
+            } else {
+                res.status(204).send("Nenhum resultado encontrado!")
+            }
+        }).catch(
+            function (erro) {
+                console.log(erro);
+                console.log("Houve um erro ao realizar a consulta! Erro: ", erro.sqlMessage);
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+}
+
+function entrar(req, res) {
+    var cpf = req.body.cpf;
+    var senha = req.body.senha;
+
+    if (cpf == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (senha == undefined) {
+        res.status(400).send("Sua senha está indefinida!");
+    } else {
+        usuarioModel.entrar(cpf, senha)
+            .then(
+                function (resultado) {
+                    console.log(`\nResultados encontrados: ${resultado.length}`);
+                    console.log(`Resultados: ${JSON.stringify(resultado)}`); // transforma JSON em String
+
+                    if (resultado.length == 1) {
+                        console.log(resultado);
+                        res.json(resultado[0]);
+                    } else if (resultado.length == 0) {
+                        res.status(403).send("Email e/ou senha inválido(s)");
+                    } else {
+                        res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+                    }
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+
+}
+
+function cadastrar(req, res) {
+    var nome = req.body.nome;
+    var sobrenome = req.body.sobrenome;
+    var cpf= req.body.cpf;
+    var senha = req.body.senha;
+
+    if (nome == undefined) {
+        res.status(400).send("Seu nome está undefined!");
+    } else if (sobrenome == undefined) {
+        res.status(400).send("Seu  sobrenome está undefined!");
+    } else if (cpf == undefined) {
+        res.status(400).send("Sua cpf está undefined!");
+    }else if (senha == undefined) {
+            res.status(400).send("Sua senha está undefined!");
+    } else {
+        usuarioModel.cadastrar(nome, sobrenome, cpf, senha)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
+module.exports = {
+    entrar,
+    cadastrar,
+    listar,
+    testar
+}
