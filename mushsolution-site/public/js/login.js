4d53d768f25d1290da9673b05e0f1959e0dd0980
Gabriel Oliveira Aguiar <gabriel.osilva@bandtec.com.br>
2021-12-06 15:27:05 -0300

    Minimal fixes

--- mushsolution-site/public/js/login.js
+++ mushsolution-site/public/js/login.js
@@ -77,7 +77,7 @@
             } else {
                 msg_login.style.display = 'block';
                 msg_login.style.color = '#ff0000';
-                msg_login.innerHTML = 'CNPJ ou Senha inválida. Tente novamente.';
+                msg_login.innerHTML = 'CPF ou Senha inválida. Tente novamente.';
                 console.log("Houve um erro ao tentar realizar o login!");
 
                 resposta.text().then(texto => {
