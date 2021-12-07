f058dab1ee0ebcb3d72c10fc972989e79de2a6ae
Gabriel Oliveira Aguiar <gabriel.osilva@bandtec.com.br>
2021-12-07 13:50:30 -0300

    Update UX FAQ - Correções Ortográficas - Correções Presentation

--- mushsolution-site/public/js/contato.js
+++ mushsolution-site/public/js/contato.js
@@ -1,4 +1,5 @@
 function enviar(){
+    var cpfReg = /^[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}$/;
     var nome = ipt_nome.value;
     var sobrenome = ipt_sobrenome.value;
     var empresa = ipt_empresa.value;
@@ -10,13 +11,23 @@ function enviar(){
     || email == "" || tel == "" || descricao == "" || assunto == ""){
         alert(`Preencha todos os campos!`)
     }else{
-        alert(`${nome}, sua mensagem foi enviada com sucesso!`)
-        ipt_nome.value = "";
-        ipt_sobrenome.value = "";
-        ipt_empresa.value = "";
-        ipt_email.value = "";
-        ipt_tel.value = "";
-        ipt_descricao.value = "";
-        ipt_assunto.value = "";
+        if(email.indexOf('@') != -1 && email.indexOf('.com') != -1){
+            console.log('Email valido')
+            if(empresa.match(cpfReg)){
+                alert(`${nome}, sua mensagem foi enviada com sucesso!`)
+                ipt_nome.value = "";
+                ipt_sobrenome.value = "";
+                ipt_empresa.value = "";
+                ipt_email.value = "";
+                ipt_tel.value = "";
+                ipt_descricao.value = "";
+                ipt_assunto.value = "";
+            }else{
+                alert('Digite um CPF válido!')
+            }
+        }else{
+            console.log('Email invalido')
+            alert('Digite um email válido!')
+        }
     }
 }
